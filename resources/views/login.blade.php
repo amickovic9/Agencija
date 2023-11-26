3631587e7906cdb4881d901c6cc406a3ea1c0fb8
Aleksandar Ćirković <116209910+cireli@users.noreply.github.com>
2023-11-26 18:41:41 +0100

    Sredjena forma za prijavu i registraciju

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ulogujte se</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>Prijava | Registracija</title>
+    <link href="/css/login.css" rel="stylesheet">
     <style>
         /* Dodatni stil za margine između elemenata */
         form {
@@ -15,45 +15,52 @@
     </style>
 </head>
 <body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Registracija</h2>
-                <form action="/register" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="name">Korisničko ime:</label>
-                        <input type="text" class="form-control" name="name" id="name">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="text" class="form-control" name="email" id="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input type="password" class="form-control" name="password" id="password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Registruj se</button>
-                </form>
-            </div>
-            <div class="col-md-6">
-                <h2>Logovanje</h2>
-                <form action="/login" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="loginname">Korisničko ime:</label>
-                        <input type="text" class="form-control" name="loginname" id="loginname">
-                    </div>
-                    <div class="form-group">
-                        <label for="loginpassword">Password:</label>
-                        <input type="password" class="form-control" name="loginpassword" id="loginpassword">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Uloguj se</button>
-                </form>
-            </div>
+<div class="container" id="container">
+      <div class="form-container sign-up">
+        <form action="/register" method="post">
+        @csrf
+          <h1>Kreiraj nalog</h1>
+          <br />
+          <input type="text" name="name" id="name" placeholder="Korisnicko Ime" />
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <input type="password" name="password" id="password" placeholder="Lozinka" />
+          <button type="submit">Registracija</button>
+        </form>
+      </div>
+      <div class="form-container sign-in">
+        <form action="/login" method="post">
+        @csrf
+          <h1>Prijava</h1>
+          <br />
+          <input type="text" placeholder="Korisnicko ime" name="loginname" id="loginname"/>
+          <input type="password" placeholder="Lozinka" name="loginpassword" id="loginpassword"/>
+          <a href="#">Zaboravljena lozinka?</a>
+          <button type="submit">Prijavi se</button>
+        </form>
+      </div>
+      <div class="toggle-container">
+        <div class="toggle">
+          <div class="toggle-panel toggle-left">
+            <h1>Dobrodošao nazad!</h1>
+            <p>
+              Unesi svoje podatke, kako bi mogao da koristiš sve mogućnosti
+              našeg sajta
+            </p>
+            <button class="hidden" id="login">Prijavi se</button>
+          </div>
+          <div class="toggle-panel toggle-right">
+            <h1>Pozdrav!</h1>
+            <p>
+              Registruj se sa svojim podacima, kako bi mogao da zakažeš svoje
+              putovanje na vreme!
+            </p>
+            <button class="hidden" id="register">Registracija</button>
+          </div>
         </div>
+      </div>
     </div>
 
+    <script src="/js/script.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
