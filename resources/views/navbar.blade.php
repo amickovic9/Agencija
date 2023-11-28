f95822304bd700e5d0129b00ce8a099feab7439e
Cope01 <petarurosevic4@gmail.com>
2023-11-28 18:02:28 +0100

    Navbar je sad fixed i promenjen je layout stvari
    Dodato da navbar uvek prati usera i ostaje na vrhu stranice dok se skroluje
    Promenjeno je gde stoje stvari u navbaru, jedne na levoj strani, druge na desnoj strani navbara

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,70 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Agencija</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/offers">Ponude</a>
-      </li>
-      
-      
-      
-      @auth
-      <li class="nav-item">
-        <a class="nav-link" href="/myReservations">Moje rezervacije</a>
-      </li>
-      
-       @if(Auth::user()->is_admin)
-       <li class="nav-item">
-        <a class="nav-link" href="/create-offer">Oglasi ponudu</a>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link" href="/admin">Admin panel</a>
-      </li>
-      
-     @endif
-     @endauth
-      <li class="nav-item">
-        <a class="nav-link" href="/gallery">Galerija</a>
-      </li>
-      @auth
-      <li class="nav-item">
-        <a class="nav-link" href="/logout">Logout</a>
-      </li>
-      
-      @else
-      <li class="nav-item">
-        <a class="nav-link" href="/login">Login</a>
-      </li>
-      @endauth
-      <li class="nav-item">
-        <a class="nav-link" href="/about">O nama</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-@if(session('success'))
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-    </div>
-@endif
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/offers">Ponude</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/gallery">Galerija</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                @auth
+                    @if(Auth::user()->is_admin)
+                        <li class="nav-item">
+                            <a class="nav-link" href="/create-offer">Oglasi ponudu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin">Admin panel</a>
+                        </li>
+                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">O nama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                @endauth
+            </ul>
+        </div>
+    </nav>
+    @if(session('success'))
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
