86e11cf1f77fddd52982148e301afa2166ec21d2
Aleksandar <amickovic9@gmail.com>
2023-11-29 22:22:33 +0100

    Galerija uradjena

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,32 +1,25 @@
 @include('navbar');
-<h1>Galerija</h1>
-<form action="/gallery/upload" method="POST" enctype="multipart/form-data">
-    @csrf 
-    <input type="file" name="photo" required>      
-    <br>
-    <br>
-    <button type='submit'>Dodajte vasu fotografiju u galeriju</button>
-</form>
-
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="{{ Storage::url('gallery/aktivna.jpg') }}" class="d-block w-100" style=" max-`width: 70%; height: 70%; margin: 0 auto;">
-        </div>
-        @foreach ($photos as $photo)
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ Storage::url('gallery/' . $photo['photo']) }}" style="width: 70%; height: 70%; margin: 0 auto;">
-            </div>  
-        @endforeach
-    </div>
-    
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/gallery.css">
+    <title>Document</title>
+</head>
+<body class="gallery-page">
+<div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+    @foreach ($photos as $photo)
+        <img class="image" src="{{ Storage::url('gallery/' . $photo['photo']) }}" draggable="false" />
+        
+    @endforeach
+  
 </div>
+
+
+<script src="../js/gallery.js"></script>
+
+
+</body>
+</html>
