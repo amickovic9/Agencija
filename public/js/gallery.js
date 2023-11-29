8860f7f0bc51a7a2e2a6e471ace96eb4d9b5d941
Cope01 <petarurosevic4@gmail.com>
2023-11-30 00:50:06 +0100

    Updateovan gallery.js
    Updateovan gallery.js, ubaceno dosta novih funkcija poput toga da su slike u loop-u, na koliko ce da nestaju i gde. i dodati event listeneri za buttone na dnu stranice

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,45 +1,87 @@
-const track = document.getElementById("image-track");
+document.addEventListener("DOMContentLoaded", () => {
+  const track = document.getElementById("image-track");
+  const images = track.getElementsByClassName("image");
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+  const firstImage = images[0].cloneNode(true);
+  const lastImage = images[images.length - 1].cloneNode(true);
+  track.prepend(lastImage);
+  track.append(firstImage);
 
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
+  track.dataset.percentage = -100 / (images.length + 2); 
+  track.style.transform = `translate(${track.dataset.percentage}%, -50%)`;
+
+  window.onmousedown = e => handleOnDown(e, track);
+  window.ontouchstart = e => handleOnDown(e.touches[0], track);
+  window.onmouseup = () => handleOnUp(track);
+  window.ontouchend = () => handleOnUp(track);
+  window.onmousemove = e => handleOnMove(e, track, images);
+  window.ontouchmove = e => handleOnMove(e.touches[0], track, images);
+});
+
+document.getElementById('uploadBtn').addEventListener('click', function() {
+  document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', function() {
+  var fileName = this.files[0].name;
+  document.getElementById('uploadBtn').textContent = fileName;
+});
+
+function handleOnDown(e, track) {
+  track.dataset.mouseDownAt = e.clientX;
+}
+
+function handleOnUp(track) {
+  track.dataset.mouseDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
+function handleOnMove(e, track, images) {
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100;
+  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+  let nextPercentage = nextPercentageUnconstrained;
+
+  if (nextPercentage >= 0) {
+      track.style.transition = 'none';
+      nextPercentage = -100 * (images.length - 1) / (images.length + 2);
+  } else if (nextPercentage <= -100 * images.length / (images.length + 2)) {
+      track.style.transition = 'none';
+      nextPercentage = -100 / (images.length + 2);
+  } else {
+      track.style.transition = '';
   }
+
+  track.dataset.percentage = nextPercentage;
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+  updateImagesOpacityAndPosition(images, nextPercentage);
 }
 
-/* -- Had to add extra lines for touch events -- */
+function updateImagesOpacityAndPosition(images, nextPercentage) {
+  for (const image of images) {
+      const rect = image.getBoundingClientRect();
+      const fadeStartLeft = 150;
+      const fadeEndLeft = 100;
+      const fadeStartRight = window.innerWidth - 150;
+      const fadeEndRight = window.innerWidth - 100;
+      let opacity = 1;
 
-window.onmousedown = e => handleOnDown(e);
+      if (rect.left < fadeStartLeft) {
+          opacity = (rect.left - fadeEndLeft) / (fadeStartLeft - fadeEndLeft);
+          opacity = Math.max(0, opacity);
+      }
 
-window.ontouchstart = e => handleOnDown(e.touches[0]);
+      if (rect.right > fadeStartRight) {
+          opacity = (fadeEndRight - rect.right) / (fadeEndRight - fadeStartRight);
+          opacity = Math.max(0, opacity);
+      }
+      opacity = Math.min(1, opacity);
 
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
+      image.style.opacity = opacity;
+  }
+}
