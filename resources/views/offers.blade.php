eea0d5403b22b45174c8e964376addce3e3b46a0
Aleksandar Ćirković <116209910+cireli@users.noreply.github.com>
2023-12-08 17:10:54 +0100

    sve ponude update

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -1,29 +1,37 @@
 @include('navbar')
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head> 
+
 <link rel="stylesheet" href="/css/offersprva.css">
-<body style="background-image: url('/img/eeee.jpg'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
-    <h1 style="text-align:center; color:#ffffff;"><span class="glowing-text">SVE PONUDE NA JEDNOM MESTU</span></h1>
-    <div class="container-fluid mt-0 mb-0">
+<body style="background-image: url('/img/3.png'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
+<div class="h1-title">    
+<h1 style="text-align:center; color:white;"><span class="glowing-text">SVE PONUDE NA JEDNOM MESTU</span></h1>
+</div> 
+<div class="container-fluid">
         <div>
             <div class="form-container" style="display: block;">
                 <form action="/offers/search" method="get" class="mb-4">
                     @csrf
                     <div class="form-row">
                         <div class="col-md-4 mb-3">
-                            <label for="destinacija" style="color: #5A5A5A; font-weight: bold;">Pretraži ponude po destinaciji:</label>
+                            <label for="destinacija" style="color: white; ">Pretraži ponude po destinaciji:</label>
                             <input type="text" class="form-control" id="destinacija" name="destinacija" value="{{ request()->input('destinacija') }}">
                         </div>
                         <div class="col-md-8 mb-3">
                             <div class="row">
                                 <div class="col">
-                                    <label for="polazak" style="color: #5A5A5A; font-weight: bold;">Datum polaska:</label>
+                                    <label for="polazak" style="color: white; ">Datum polaska:</label>
                                     <input type="date" class="form-control" id="polazak" name="polazak" value="{{ request()->input('polazak') }}" min="{{ now()->format('Y-m-d') }}" max="{{ now()->addYears(4)->format('Y-m-d') }}">
                                 </div>
                                 <div class="col">
-                                    <label for="povratak" style="color: #5A5A5A; font-weight: bold;">Datum povratka:</label>
+                                    <label for="povratak" style="color: white; ">Datum povratka:</label>
                                     <input type="date" class="form-control" id="povratak" name="povratak" value="{{ request()->input('povratak') }}" min="{{ now()->format('Y-m-d') }}" max="{{ now()->addYears(4)->format('Y-m-d') }}">
                                 </div>
                                 <div class="col">
-                                    <label for="sort" style="color: #5A5A5A; font-weight: bold;">Sortiraj po:</label>
+                                    <label for="sort" style="color: white; ">Sortiraj po:</label>
                                         <select class="form-control" id="sort" name="sort">
                                             <option value="newest" {{ request('sort') == 'newest' ? 'selected' : '' }}>Najnovije</option>
                                             <option value="priceAsc" {{ request('sort') == 'priceAsc' ? 'selected' : '' }}>Cena - rastuće</option>
