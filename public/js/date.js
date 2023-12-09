cc7bb7b38b4f96a3b6f11c2f100956304799c58a
Cope01 <petarurosevic4@gmail.com>
2023-12-10 00:42:17 +0100

    Update date.js
    Dodato da ako se ukuca u polja za cenu ili mesta vrednost koja je visa ili niza od dozvoljene ono automatski vrati na najblizu dozvoljenu vrednost

--- public/js/date.js
+++ public/js/date.js
@@ -6,3 +6,29 @@ document.getElementById('polazak').addEventListener('change', function() {
         returnInput.value = departureDate;
     }
 });
+document.getElementById('polazak').addEventListener('change', function() {
+    var departureDate = this.value;
+    var returnInput = document.getElementById('povratak');
+    returnInput.min = departureDate;
+    if (returnInput.value < departureDate) {
+        returnInput.value = departureDate;
+    }
+});
+  document.addEventListener('DOMContentLoaded', function() {
+    const enforceRange = (input, min, max) => {
+      input.addEventListener('blur', () => {
+        let value = parseInt(input.value, 10);
+        if (value < min) {
+          input.value = min;
+        } else if (value > max) {
+          input.value = max;
+        }
+      });
+    };
+
+    const brojMestaInput = document.querySelector('input[name="broj_mesta"]');
+    const cenaInput = document.querySelector('input[name="cena"]');
+
+    enforceRange(brojMestaInput, 1, 100);
+    enforceRange(cenaInput, 1, 100000);
+});
