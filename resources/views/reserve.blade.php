8689494e45db9b56dba6be941f05e9e2c9aeabe1
Aleksandar Ćirković <116209910+cireli@users.noreply.github.com>
2023-12-10 14:32:35 +0100

    kompletna strana prilikom rezervacije

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -8,7 +8,45 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+   
+  <script>
+    function proveriFormu() {
+      var form = document.getElementById("reservationForm");
+      var userName = form.elements["user_name"].value;
+      var email = form.elements["email"].value;
+      var phoneNumber = form.elements["broj_telefona"].value;
+      var numberOfPeople = form.elements["broj_osoba"].value;
+      var note = form.elements["napomena"].value;
 
+      
+      if (userName.trim() === "") {
+        prikaziGresku("*Unesite korisničko ime!");
+        return false;
+      }
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        prikaziGresku("*Unesite validnu e-mail adresu!");
+        return false;
+      }
+      var phoneNumberRegex = /^\+381\s6\d\s\d{2}\s\d{2}\s\d{2}$|^\+381\s6\d\s\d{2}\s\d{2}\s\d{3}$/;
+      if (!phoneNumberRegex.test(phoneNumber)) {
+          prikaziGresku("*Unesite validan broj telefona u formatu +381 6x xx xx xx ili +381 6x xx xx xxx!");
+          return false;
+      }
+
+      if (isNaN(numberOfPeople) || numberOfPeople <= 0) {
+        prikaziGresku("*Unesite validan broj osoba!");
+        return false;
+      }
+      return true;
+    }
+
+    function prikaziGresku(message) {
+      var errorMessage = document.getElementById("error-message");
+      errorMessage.innerHTML = message;
+      errorMessage.style.display = "block";
+    }
+  </script>
 </head>
 <body>
 
@@ -32,19 +70,19 @@
     </div>
 
 
-        <div class="reservation-form">
-            <form action="/reserve/{{$offer['id']}}" method="post">
-                @csrf
-                <h1>Rezervisi putovanje -<b> {{$offer['destinacija']}}</b></h1>
-                <input class="input" type="text" name="user_name" placeholder="Korisničko ime"><br><br>
-                <input class="input" type="email" name="email" placeholder="Email"><br><br>
-                <input class="input" type="number" name="broj_telefona" placeholder="Broj teledona"><br><br>
-                <input class="input" type="number" name="broj_osoba" placeholder="Broj osoba"><br><br>
-                <input class="input" type="text" name="napomena" placeholder="Napomena"><br><br>
-                <button type="submit" class="button"><span>Rezerviši putovanje</span></button>
-        
-            </form>
-        </div>
+    <div class="reservation-form">
+    <form id="reservationForm" action="/reserve/{{$offer['id']}}" method="post" onsubmit="return proveriFormu()">
+      @csrf
+      <h1>Rezervisi putovanje - <b>{{$offer['destinacija']}}</b></h1>
+      <input class="input" type="text" name="user_name" placeholder="Korisničko ime" ><br><br>
+      <input class="input" type="email" name="email" placeholder="Email " ><br><br>
+      <input class="input" type="tel" name="broj_telefona" placeholder="Broj telefona +381 6x xx xx xxx" size="13" ><br><br>
+      <input class="input" type="number" name="broj_osoba" placeholder="Broj osoba" min="1" max="{{$offer['broj_mesta']}}"><br><br>
+      <input class="input" type="text" name="napomena" placeholder="Napomena" ><br><br>
+      <button type="submit" class="button" id="reserveButton"><span>Rezerviši putovanje</span></button>
+      <div id="error-message" class="error-message"></div>
+    </form>
+  </div>
     </div>
 <section class="middle"> 
    <h1> Pogledajte i ostale destinacije koje ljudi takodje pretražuju <h1>
