5c29c92de62f8ce1e47ebcdccf8ee6c7532ff71d
Cope01 <petarurosevic4@gmail.com>
2024-01-13 19:09:32 +0100

    Update footer.blade.php
    Sklonjene neke nebitne stvari iz footera

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,17 +5,6 @@
 <footer>
 <svg viewBox="0 0 120 28">
  <defs> 
-
-   
-   <filter id="goo">
-      <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur" />
-      <feColorMatrix in="blur" mode="matrix" values="
-           1 0 0 0 0  
-           0 1 0 0 0  
-           0 0 1 0 0  
-           0 0 0 13 -9" result="goo" />
-      <feBlend in="SourceGraphic" in2="goo" />
-  	</filter>
      <path id="wave" d="M 0,10 C 30,10 30,15 60,15 90,15 90,10 120,10 150,10 150,15 180,15 210,15 210,10 240,10 v 28 h -240 z" />
   </defs> 
 
