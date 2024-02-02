be73ec4ffa5a06ba48b845b33013c68df8ecddad
Aleksandar <amickovic9@gmail.com>
2024-02-02 01:53:38 +0100

    email pw notification

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -38,7 +38,14 @@
         </button>
     </div>
 @endif
-
+@if(session('danger'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('danger') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
 <script>
     document.getElementById('menuToggle').addEventListener('click', function() {
         var nav1 = document.querySelector('.nav1');
