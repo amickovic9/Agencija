3e43c6acc3ae44273417e6481e6a63438015067c
Aleksandar <amickovic9@gmail.com>
2024-02-02 01:48:16 +0100

    ubacen popup za pogresnu sifru ili username

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Routing\Redirector;
 
 use App\Models\User;
@@ -9,35 +10,34 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
-       $fields= $request->validate([
-        'name' => ['required','min:3', Rule::unique('users','name')],
-        'email' => ['required','email', Rule::unique('users','email')],
-        'password' => ['required','min:8'],
-       ]);
-       $fields['password'] = bcrypt($fields['password']);
-       $user = User::create($fields);
-       return redirect('/login')->with('success', 'Uspesno ste se registrovali!');
-
-
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => ['required', 'min:3', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8'],
+        ]);
+        $fields['password'] = bcrypt($fields['password']);
+        $user = User::create($fields);
+        return redirect('/login')->with('success', 'Uspesno ste se registrovali!');
     }
 
-    public function logout(){
+    public function logout()
+    {
         auth()->logout();
         return redirect('/');
     }
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $fields = $request->validate([
-            'loginname' =>'required',
-            'loginpassword' =>'required'
+            'loginname' => 'required',
+            'loginpassword' => 'required'
         ]);
-        if(auth()->attempt(['name' => $fields['loginname'], 'password' => $fields['loginpassword']])){
+        if (auth()->attempt(['name' => $fields['loginname'], 'password' => $fields['loginpassword']])) {
             $request->session()->regenerate();
+        } else {
+            return redirect('/login')->with('danger', 'Pogresno korisnicko ime ili password!');
         }
-        else{
-            return redirect('/login');
-        }
-               return redirect('/')->with('success', ' Uspesno ste se ulogovali!');
-
+        return redirect('/')->with('success', ' Uspesno ste se ulogovali!');
     }
 }
